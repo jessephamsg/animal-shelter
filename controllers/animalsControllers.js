7c4e2e25c11385410085dab99ed0ede057c49709
Tam Pham <tamphamnguyen92@gmail.com>
2020-07-25 14:02:46 +0800

    added getOne

--- controllers/animalsControllers.js
+++ controllers/animalsControllers.js
@@ -2,7 +2,20 @@ const repositories = require('../repositories/animalRepositories');
 
 module.exports = {
     async getAll (req, res) {
-        const data = await repositories.getAll();
-        res.send(data);
+        try {
+            const data = await repositories.getAll();
+            res.send(data);
+        } catch {
+            res.send(`failed to fetch data`);
+        }
     },
+    async getOne (req, res) {
+        const index = req.params.index;
+        try {
+            const data = await repositories.getOne(index);
+            res.send(data);
+        } catch {
+            res.send(`failed to fetch objectID ${index}`);
+        }
+    }
 }
--- repositories/animalRepositories.js
+++ repositories/animalRepositories.js
@@ -1,11 +1,17 @@
 const db = require('../db/db');
+const ObjectId = require('mongodb').ObjectID;
 
 module.exports = {
     async getAll () {
         const data = await db.animals.find().toArray();
         return data
     },
-    async getOne () {
-        
+    async getOne (index) {
+        console.log(index);
+        const data = await db.animals.find({
+            _id: ObjectId(index)
+        }).toArray();
+        console.log(data);
+        return data
     }
 }
